3c82975f842c2f3e5c0cc3944a3faf59e28e0e3f
DavidTomas <dtalonsocalderon@gmail.com>
2024-02-24 10:39:39 +0100

    Added following features: - libs.versions.toml - BuildSrc

---
+++ buildSrc/src/main/kotlin/com/davidtomas/taskyapp/BuildVersion.kt
@@ -0,0 +1,23 @@
+import org.gradle.api.JavaVersion
+
+object BuildVersion {
+    object Environment {
+        private const val majorVersion = 1
+        private const val minorVersion = 0
+        private const val bugfixVersion = 0
+
+        const val minSdkVersion = 24
+        const val compileSdkVersion = 34
+        const val targetSdkVersion = 34
+        const val applicationId = "com.davidtomas.taskyapp"
+        const val appVersionCode = majorVersion * 1000 + minorVersion * 100 + bugfixVersion
+        const val appVersionName = "${majorVersion}.${minorVersion}.$bugfixVersion"
+        val javaVersion = JavaVersion.VERSION_17
+        const val jvmTarget = "17"
+    }
+
+    object TestEnvironment {
+        const val instrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        const val instrumentationRunnerArgs = "de.mannodermaus.junit5.AndroidJUnit5Builder"
+    }
+}
